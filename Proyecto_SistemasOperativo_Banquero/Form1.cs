1bd1e94ef7e2700c900635461b5f7a142879c081
Fernando Davalos Gutierrez <fernandodava203@gmail.com>
2024-11-25 18:11:19 -0600

    Add files via upload

---
+++ Proyecto_SistemasOperativo_Banquero/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_SistemasOperativo_Banquero
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
